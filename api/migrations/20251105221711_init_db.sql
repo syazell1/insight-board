f7dc24ea9c4b062c70e009e1f8f303ad16d1a388
syazell1 <mafty.erin01@example.com>
2025-11-09 02:36:01 +0000

    add migrations

--- api/migrations/20251105221711_init_db.sql
+++ api/migrations/20251105221711_init_db.sql
@@ -1,1 +1,18 @@
 -- Add migration script here
+CREATE TABLE users (
+  id uuid NOT NULL,
+  PRIMARY KEY(id),
+  username TEXT NOT NULL,
+  password TEXT NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NULL
+);
+
+CREATE TABLE users_tokens (
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    token TEXT NOT NULL,
+    created_at timestamptz NOT NULL,
+    user_id uuid NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
